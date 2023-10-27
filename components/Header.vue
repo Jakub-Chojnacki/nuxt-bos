9dfd67e1f541889c4a38409d3f709f99be1ae7c2
Jakub Chojnacki <60758743+Jakub-Chojnacki@users.noreply.github.com>
2023-10-27 22:29:30 +0200

    initial setup of fonts, sass and fonts

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="header">
+        <div class="header__logo-container">
+            <img src="~/assets/img/merito-logo.png" alt="Merito logo" />
+        </div>
+    </header>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss" scoped>
+.header {
+    background-color: white;
+    padding: 1em;
+
+    &__logo-container {
+        img {
+            max-width: 40px;
+        }
+    }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    "~/assets/main.scss",
+    "~/assets/_mixins.scss",
+    "~/assets/_variables.scss",
+  ],
+  modules: ['nuxt-icon', '@nuxtjs/google-fonts'],
+  googleFonts: {
+    families:{
+      Inter: [400, 700],
+    }
+  }
+});
